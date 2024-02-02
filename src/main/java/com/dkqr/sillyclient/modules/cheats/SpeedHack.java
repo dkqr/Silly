5f9ae3dc52c731a34eb31865f0ca576f0bee0b51
dkqr <BraydonCoombz@gmail.com>
2024-02-01 18:43:16 -0800

    Fixed speedhack :3

--- src/main/java/com/dkqr/sillyclient/modules/cheats/SpeedHack.java
+++ src/main/java/com/dkqr/sillyclient/modules/cheats/SpeedHack.java
@@ -2,15 +2,18 @@ package com.dkqr.sillyclient.modules.cheats;
 
 import com.dkqr.sillyclient.modules.Category;
 import com.dkqr.sillyclient.modules.Module;
+import net.minecraft.util.math.Vec3d;
 
 public class SpeedHack extends Module {
     public SpeedHack(String name, Category category, int keycode) {
         super(name, category, keycode);
     }
-    public void onEnable() {
-        client.player.getAbilities().setWalkSpeed(0.07F);
-    }
-    public void onDisable() {
-        client.player.getAbilities().setWalkSpeed(0.1F);
+    public void onTick() {
+        if (client.player.isSneaking() || client.player.forwardSpeed == 0 && client.player.sidewaysSpeed == 0)
+            return;
+        if (!client.player.isOnGround())
+            return;
+        Vec3d plrVel = client.player.getVelocity();
+        client.player.setVelocity(plrVel.x * 1.8, 0.5, plrVel.z * 1.8);
     }
 }
